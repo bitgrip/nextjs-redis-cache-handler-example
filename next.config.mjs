d7b2ff812e1fc5867b142a38c33f1fe5cc6ec252
Jan-Henrik Hempel <jan-henrik.hempel@bitgrip.de>
2025-02-21 08:23:03 +0100

    Setup plain nextjs.

---
+++ next.config.mjs
@@ -0,0 +1,9 @@
+/** @type {import('next').NextConfig} */
+const nextConfig = {
+  experimental: {
+    // Enable experimental cache directive
+    cacheHandler: true,
+  }
+}
+
+export default nextConfig;
